#2 env features:  - added reward class (fixed reward on load power  - introduced normalization based on fixed limits
ddpg-example-file:
 - Using 2 Node example
 - constant action in loop
 - plotting denomalized outputs + power/ref

pre_investigations/python/Interface/env_dare.py
@@ -8,17 +8,52 @@ import scipy
 from pre_investigations.python.Interface.util import NodeConstructor
 
 
+class Reward:
+    def __init__(self, params, limits, refs, gamma=0):
+        self.params = params
+        self.limits = limits
+        self.refs = refs
+        self.gamma = gamma
+
+    def rew_function(self, obs):
+        """
+
+        :param obs: (if normalized=True) normalized observations
+        :return: reward to get specific power via load
+        """
+
+        # hard coded!!! toDO make flexible depending on e.g. history
+        #P_load = ((obs[4] + obs[5])**2)*self.params['R_load']
+
+        # denormalize
+        i_T1 = obs[4]*self.limits['i_lim']
+        i_T2 = obs[5]*self.limits['i_lim']
+        P_load = ((i_T1 + i_T2) ** 2) * self.params['R_load']
+
+        # to normalize, we take the Power by i_max through the load
+        P_max = (2*self.limits['i_lim'])**2  * self.params['R_load']
+
+        # if error = P_max -> rew = -1; if error = 0 -> rew = +1
+        # gamma normalization for training
+        rew = (1 - 2*np.sqrt(np.abs(P_load - self.refs)/P_max)) * (1 - self.gamma)
+
+        return rew
+
+
+
+
 class Env_DARE(gym.Env):
 
     TIMEOUT = 300
 
-    def __init__(self, CM, ts, parameter, x0, time_start=0):
+    def __init__(self, CM, ts, parameter, x0, limits=None, refs=None, gamma=0, time_start=0):
 
+        # toDo shift gamma to env wrapper (or kwargs?)
         super(Env_DARE, self).__init__()
 
 
         power_grid = NodeConstructor(2, 1, parameter, CM=CM)  # 2 Source with 2 connections
-        power_grid.draw_graph()
+        #power_grid.draw_graph()
 
         A, B, C, D = power_grid.get_sys()
         # discretize
@@ -34,6 +69,19 @@ class Env_DARE(gym.Env):
         self.sys_d = control.ss(A_d, B_d, C_d, 0, dt=True)
 
         self.number_of_steps = 0
+        self.refs = refs
+
+        if limits is None:
+            self.i_lim = None
+            self.v_lim = None
+        else:
+            self.i_lim = limits['i_lim']
+            self.v_lim = limits['v_lim']
+            # toDo make flexible depening on state
+            self.norm_array = np.array([self.i_lim, self.v_lim, self.i_lim, self.v_lim, self.i_lim, self.i_lim])
+
+        self.rew = Reward(parameter, limits, self.refs, gamma)
+
 
         self.action_space = gym.spaces.Box(
             low=-1,
@@ -73,14 +121,18 @@ class Env_DARE(gym.Env):
         else:
             reward = 0.0
 
-        return self._simulate(action), reward, is_done, {}
+        obs = self._simulate(action)
+
+        reward = self.rew.rew_function(obs)
+
+        return obs, reward, is_done, {}
 
 
     def _simulate(self,action, normalize=True):
         # actions is "doubled" since in sipy ltisys.py L.3505 last is discarded
         # toDo more performant stepwise interaction possible?
         T, yout, xout = control.forced_response(self.sys_d, T=self.sim_time_interval,
-                                                       U=np.array([action.squeeze(),action.squeeze()]),
+                                                       U=np.array([action.squeeze(),action.squeeze()]).T,
                                                        X0=self._state,
                                                        return_x=True, squeeze=True)
 
@@ -89,7 +141,7 @@ class Env_DARE(gym.Env):
 
         if normalize:
             # toDo
-            return (self._state/1)
+            return (self._state/self.norm_array)
         else:
             return self._state
 

pre_investigations/python/Interface/sb3_ddpg_2inv_1load.py
@@ -1,12 +1,62 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 from stable_baselines3 import DDPG
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 
-from pre_investigations.python.Interface.env_2_nodes import Env_2_Nodes
+from pre_investigations.python.Interface.env_dare import Env_DARE
 
-env = Env_2_Nodes()
+# define time vector
+ts = 1e-4
+t_end = 0.01
+steps = int(1/ts)
+t = np.arange(0, t_end+ts, ts)
+num_samples = len(t)
+
+# network params
+R = 0.4
+L = 2.3e-3
+C = 10e-6
+LT = 2.3e-3
+R_load = 14
+
+# limits for normalization
+i_lim = 20
+v_lim = 600
+
+limits = dict()
+limits['i_lim'] = i_lim
+limits['v_lim'] = v_lim
+
+# x0 = np.array([i10, v10, i20, v20, iT10, iT20])
+x0 = [0, 0, 0, 0, 0, 0]
+
+vi1 = 200
+vi2 = 200
+
+parameter = dict()
+parameter['R_source'] = R
+parameter['L_source'] = L
+parameter['C_source'] = C
+parameter['L_cabel'] = LT
+parameter['R_cabel'] = R
+parameter['R_load'] = R_load
+
+# define CM
+CM = np.array([[0, 0, 1],
+               [0, 0, 2],
+               [-1, -2, 0]])
+
+
+
+ref = 200 # W
+
+env = Env_DARE(CM, ts, parameter, x0, limits, ref)
+
+
+"""
 # The noise objects for DDPG
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
@@ -19,10 +69,86 @@ env = model.get_env()
 #del model # remove to demonstrate saving and loading
 
 #model = DDPG.load("ddpg_pendulum")
+"""
+
+v1_list = []
+
 
 obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
+obs_matrix = np.array(obs)
+v1_list.append(obs[1])
+for i in range(num_samples-1):
+    #action, _states = model.predict(obs)
+    action = np.array([[vi1], [vi2]])
+    #action = np.array([action.squeeze(),action.squeeze()])#np.array([[vi1], [vi2]])
     obs, rewards, dones, info = env.step(action)
-    print(obs)
-    env.render()
+    obs_matrix = np.vstack((obs_matrix, obs))
+    v1_list.append(obs[1])
+    #env.render()
+
+
+# x0 = np.array([i10, v10, iT10, i20, v20, iT20])
+
+
+# denormalize!
+obs_matrix_denorm = obs_matrix*env.norm_array
+
+fig, ax1 = plt.subplots(2, 2)
+
+ax1[0, 0].step(t, obs_matrix_denorm[:, 1], 'r', label='v1')
+# plt.plot(t,result[:steps,0], label = 'i1')
+ax1[0, 0].set_xlabel(r'$t\,/\,\mathrm{s}$')
+ax1[0, 0].set_ylabel('$v_{\mathrm{1}}\,/\,\mathrm{V}$')
+# plt.title('{}'.format())
+ax1[0, 0].legend()
+ax1[0, 0].grid()
+#ax1[0].set_ylim([0, 340])
+
+ax1[0, 1].step(t, obs_matrix_denorm[:, 0], 'r', label='i1')
+ax1[0, 1].step(t, obs_matrix_denorm[:, 4], '--b', label='iT1')
+# plt.plot(t,result[:steps,0], label = 'i1')
+ax1[0, 1].set_xlabel(r'$t\,/\,\mathrm{s}$')
+ax1[0, 1].set_ylabel('$i_{\mathrm{}}\,/\,\mathrm{A}$')
+# plt.title('{}'.format())
+ax1[0, 1].legend()
+ax1[0, 1].grid()
+#ax1[0].set_ylim([0, 340])
+
+
+ax1[1, 0].step(t, obs_matrix_denorm[:, 3], 'r', label='v2')
+# plt.plot(t,result[:steps,0], label = 'i1')
+ax1[1, 0].set_xlabel(r'$t\,/\,\mathrm{s}$')
+ax1[1, 0].set_ylabel('$v_{\mathrm{2}}\,/\,\mathrm{V}$')
+# plt.title('{}'.format())
+ax1[1, 0].legend()
+ax1[1, 0].grid()
+#ax1[0].set_ylim([0, 340])
+
+ax1[1, 1].step(t, obs_matrix_denorm[:, 2], 'r', label='i2')
+ax1[1, 1].step(t, obs_matrix_denorm[:, 5], '--b', label='iT2')
+# plt.plot(t,result[:steps,0], label = 'i1')
+ax1[1, 1].set_xlabel(r'$t\,/\,\mathrm{s}$')
+ax1[1, 1].set_ylabel('$i_{\mathrm{}}\,/\,\mathrm{A}$')
+# plt.title('{}'.format())
+ax1[1, 1].legend()
+ax1[1, 1].grid()
+#ax1[0].set_ylim([0, 340])
+plt.show()
+
+P_ref = [ref] * len(t)
+P_load = (obs_matrix_denorm[:, 4]+obs_matrix_denorm[:, 5])*parameter["R_load"]
+# plot power via Load
+plt.step(t, P_load, 'b', label='P_{load}')
+plt.step(t, P_ref, '--', color='gray', label='P_{ref}')
+plt.xlabel(r'$t\,/\,\mathrm{s}$')
+plt.ylabel('$P_{\mathrm{}}\,/\,\mathrm{W}$')
+# plt.title('{}'.format())
+plt.legend()
+plt.grid()
+#ax1[0].set_ylim([0, 340])
+plt.show()
+
+
+
+
+