808843227be38d75e1bbdb77cbf9a4c1be721f7c
webbah <Dwupet09!>
2022-03-09 07:54:12 +0100

    #8 - reduced solver variants first VM experiment

--- pre_investigations/python/solver_investigations/timing_experiment.py
+++ pre_investigations/python/solver_investigations/timing_experiment.py
@@ -26,11 +26,11 @@ num_cm = 3
 t_s_used = 1e-4
 t_end_used = [0.03]  # , 0.1]
 num_nodes_used = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-                  29, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]#, 105, 110, 115, 120, 125, 130]
+                  29, 30, 35, 40, 45, 50, 55, 60]#, 65, 70, 75, 80, 85, 90, 95, 100]#, 105, 110, 115, 120, 125, 130]
 #used_methode = ['control.py', 'env_standalone', 'scipy_ode', 'scipy_odeint', 'scipy_solve_ivp', 'env_agent_interaction']
 #used_methode_args = ['discrete', '', {'integrator': 'lsoda', 'methode': 'bdf'}, 'LSODA', 'LSODA', '']
-used_methode = ['control.py', 'scipy_solve_ivp', 'scipy_solve_ivp', 'scipy_solve_ivp']#, 'scipy_solve_ivp']#, 'env_standalone', 'scipy_solve_ivp']
-used_methode_args = ['','LSODA' , 'RK45', 'RK23']#, 'RK23']#, '', 'RK23']
+used_methode = ['control.py', 'scipy_solve_ivp', 'scipy_solve_ivp']#, 'scipy_solve_ivp']#, 'env_standalone', 'scipy_solve_ivp']
+used_methode_args = ['' , 'RK45', 'RK23']#, 'RK23']#, '', 'RK23']
 
 # store HPs to json
 python_simulation_HPs = {
