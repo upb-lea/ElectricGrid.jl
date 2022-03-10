7eecb300466f2bfe54dc4c10d7b74abda1174abf
webbah <Dwupet09!>
2022-03-10 07:30:54 +0100

    #8 - only control!

--- pre_investigations/python/solver_investigations/timing_experiment.py
+++ pre_investigations/python/solver_investigations/timing_experiment.py
@@ -31,8 +31,8 @@ num_nodes_used = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18, 19
                   29, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]#, 105, 110, 115, 120, 125, 130]
 #used_methode = ['control.py', 'env_standalone', 'scipy_ode', 'scipy_odeint', 'scipy_solve_ivp', 'env_agent_interaction']
 #used_methode_args = ['discrete', '', {'integrator': 'lsoda', 'methode': 'bdf'}, 'LSODA', 'LSODA', '']
-used_methode = ['control.py', 'scipy_solve_ivp', 'scipy_solve_ivp']#, 'scipy_solve_ivp']#, 'env_standalone', 'scipy_solve_ivp']
-used_methode_args = ['' , 'RK45', 'RK23']#, 'RK23']#, '', 'RK23']
+used_methode = ['control.py']#, 'scipy_solve_ivp', 'scipy_solve_ivp']#, 'scipy_solve_ivp']#, 'env_standalone', 'scipy_solve_ivp']
+used_methode_args = ['' ]#, 'RK45', 'RK23']#, 'RK23']#, '', 'RK23']
 
 # store HPs to json
 python_simulation_HPs = {
