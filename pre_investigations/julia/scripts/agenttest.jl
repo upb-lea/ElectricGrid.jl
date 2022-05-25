32ff56e175a0c6d7e3f229dc20ea033a153df334
cvikas <cvikas@mail.uni-paderborn.de>
2022-05-25 14:15:40 +0200

    #15 first commit

--- pre_investigations/julia/scripts/agenttest.jl
+++ pre_investigations/julia/scripts/agenttest.jl
@@ -1,19 +1,32 @@
 using DrWatson
 @quickactivate "MicroGridSimWithRL"
 
-using DifferentialEquations
-using Sundials
-using Plots
-using LinearAlgebra
-using ControlSystems
-using BenchmarkTools
+# using DifferentialEquations
+# using Sundials
+# using Plots
+# using LinearAlgebra
+# using ControlSystems
+# using BenchmarkTools
 using ReinforcementLearning
-using IntervalSets
+# using IntervalSets
 
 
-include(srcdir("variables.jl"))
+include(srcdir("nodeconstructor.jl"))
 include(srcdir("env.jl"))
 
+CM = [ 0.  1.
+        -1.  0.]
+
+parameters = Dict()
+parameters["source"] = [Dict("fltr" => "LCL", "R" => 0.4, "L1" => 2.3e-3, "L2" => 2.3e-3,"C" => 10e-6)]
+parameters["cable"] = [Dict("R" => 0.722, "L" => 0.955e-3, "C" => 8e-09)]
+parameters["load"] = [Dict("impedance" => "R", "R" => 14)]
+
+Grid_FC = NodeConstructor(num_source=1, num_loads=1, CM=CM, parameters=parameters)
+
+#draw_graph(Grid_FC)   ---   not yet implemented
+
+A, B, C, D = get_sys(Grid_FC)
 
 env = SimEnv(A=A, B=B, C=C)
 
