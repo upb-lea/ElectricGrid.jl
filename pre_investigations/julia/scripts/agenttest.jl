a125f0948d164cfb522f19cc1a05b878316c9e4a
cvikas <cvikas@mail.uni-paderborn.de>
2022-05-25 15:10:01 +0200

    #15 first experiment results

--- pre_investigations/julia/scripts/agenttest.jl
+++ pre_investigations/julia/scripts/agenttest.jl
@@ -3,30 +3,68 @@ using DrWatson
 
 # using DifferentialEquations
 # using Sundials
-# using Plots
+using Plots
 # using LinearAlgebra
 # using ControlSystems
 # using BenchmarkTools
 using ReinforcementLearning
+using Flux
+using StableRNGs
 # using IntervalSets
-
 
 include(srcdir("nodeconstructor.jl"))
 include(srcdir("env.jl"))
+include(srcdir("agent.jl"))
 
 CM = [ 0.  1.
         -1.  0.]
 
 parameters = Dict()
-parameters["source"] = [Dict("fltr" => "LCL", "R" => 0.4, "L1" => 2.3e-3, "L2" => 2.3e-3,"C" => 10e-6)]
+# LC filter
+parameters["source"] = [Dict("fltr" => "LC", "R" => 0.4, "L1" => 2.3e-3, "C" => 10e-6)]
 parameters["cable"] = [Dict("R" => 0.722, "L" => 0.955e-3, "C" => 8e-09)]
 parameters["load"] = [Dict("impedance" => "R", "R" => 14)]
 
-Grid_FC = NodeConstructor(num_source=1, num_loads=1, CM=CM, parameters=parameters)
+nc = NodeConstructor(num_source=1, num_loads=1, CM=CM, parameters=parameters)
 
 #draw_graph(Grid_FC)   ---   not yet implemented
 
-A, B, C, D = get_sys(Grid_FC)
+A, B, C, D = get_sys(nc)
 
-env = SimEnv(A=A, B=B, C=C)
+limits = Dict("i_lim" => 20, "v_lim" => 600)
+
+norm_array = vcat([limits[i] for j = 1:nc.num_source for i in ["i_lim", "v_lim"]], [limits["i_lim"] for i = 1:nc.num_connections] )
+norm_array = vcat( norm_array, [limits["v_lim"] for i = 1:nc.num_loads] )
+
+ns = length(A[1,:])
+na = length(B[1,:])
+
+# time step
+ts = 1e-5
+
+global env = SimEnv(A=A, B=B, C=C, norm_array=norm_array, v_dc=300, ts=rationalize(ts))
+global agent = create_agent(na, ns)
+
+
+# ----------------------------------------------------------------------------------------
+function execute_env(env::SimEnv, agent::Agent, t_len::Int, debug::Bool)
+    if debug
+        output = zeros(length(env.Ad[1,:]),t_len+1)
+    else
+        output = 0.0
+    end
+
+    RLBase.reset!(env)
+    for i = 1:t_len
+        action = agent(env)
+        env(action)
+        if debug output[:,i+1] = env.state.*env.norm_array end
+    end
+
+    return output
+end
+
+result = execute_env(env, agent, 300, true)
+
+display(plot(result[4, :]))
 
---
+++ pre_investigations/julia/src/agent.jl
@@ -0,0 +1,60 @@
+using ReinforcementLearning
+using Flux
+using StableRNGs
+
+# also in a sep src
+global rngg = StableRNG(123)
+global initt = glorot_uniform(rngg)
+
+global create_actor(na, ns) = Chain(
+    Dense(ns, 400, relu; init = initt),
+    Dense(400, 300, relu; init = initt),
+    Dense(300, na, tanh; init = initt),
+) |> gpu
+
+# gpu --> cpu? 
+
+global create_critic(na, ns) = Chain(
+    Dense(ns + na, 400, relu; init = initt),
+    Dense(400, 300, relu; init = initt),
+    Dense(300, 1; init = initt),
+) |> gpu
+
+function create_agent(na, ns)
+    Agent(
+        policy = DDPGPolicy(
+            behavior_actor = NeuralNetworkApproximator(
+                model = create_actor(na, ns),
+                optimizer = ADAM(),
+            ),
+            behavior_critic = NeuralNetworkApproximator(
+                model = create_critic(na, ns),
+                optimizer = ADAM(),
+            ),
+            target_actor = NeuralNetworkApproximator(
+                model = create_actor(na, ns),
+                optimizer = ADAM(),
+            ),
+            target_critic = NeuralNetworkApproximator(
+                model = create_critic(na, ns),
+                optimizer = ADAM(),
+            ),
+            γ = 0.99f0,
+            ρ = 0.995f0,
+            na = na,
+            batch_size = 64,
+            start_steps = 0,
+            start_policy = RandomPolicy(-1.0..1.0; rng = rngg),
+            update_after = 1000,
+            update_freq = 1,
+            act_limit = 1.0,
+            act_noise = 0.1,
+            rng = rngg,
+        ),
+        trajectory = CircularArraySARTTrajectory(
+            capacity = 100,
+            state = Vector{Float32} => (ns,),
+            action = Float32 => (na, ),
+        ),
+    )
+end
