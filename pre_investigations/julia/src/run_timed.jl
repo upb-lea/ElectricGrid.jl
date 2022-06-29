80310d3c0fd299b08918b88eedf9128752fe7611
Jan Stenner <jan.stenner@uni-paderborn.de>
2022-06-29 17:14:07 +0200

    Revert "Revert "#15 working timers with sections""
    This reverts commit 6d6e704b5bed38a307db9660fc18c052bbdf1ecd.

--- pre_investigations/julia/src/run_timed.jl
+++ pre_investigations/julia/src/run_timed.jl
@@ -1,5 +1,8 @@
+import Base: run
+
 using ReinforcementLearning
 using TimerOutputs
+using CUDA
 
 (agent::Agent)(env, timer::TimerOutput) = agent.policy(env, timer::TimerOutput)
 
@@ -7,42 +10,91 @@ function (p::DDPGPolicy)(env, timer::TimerOutput, player::Any = nothing)
     p.update_step += 1
 
     if p.update_step <= p.start_steps
-        p.start_policy(env)
+        @timeit timer "Agent calculate" begin
+            if typeof(device(p)) == Val{:cpu}
+                p.start_policy(env)
+            else
+                CUDA.@sync begin
+                    p.start_policy(env)
+                end
+            end
+        end
     else
-        D = device(p.behavior_actor)
-        s = DynamicStyle(env) == SEQUENTIAL ? state(env) : state(env, player)
-        s = Flux.unsqueeze(s, ndims(s) + 1)
-        s = send_to_device(D, s)
+        @timeit timer "Agent prepare data" begin
+            D = device(p.behavior_actor)
+            s = DynamicStyle(env) == SEQUENTIAL ? state(env) : state(env, player)
+            s = Flux.unsqueeze(s, ndims(s) + 1)
+            s = send_to_device(D, s)
+        end
+        
+        @timeit timer "Agent calculate" begin
+            if typeof(device(p)) == Val{:cpu}
+                actions = p.behavior_actor(s) |> vec
+            else
+                CUDA.@sync begin
+                    actions = p.behavior_actor(s) |> vec
+                end
+            end
+        end
 
-        actions = p.behavior_actor(s) |> vec
-
-        actions = actions |> send_to_host
-        # TODO: use alternative to clamp - to run solely on GPU
-        c = clamp.(actions .+ randn(p.rng, p.na) .* repeat([p.act_noise], p.na), -p.act_limit, p.act_limit)
-        p.na == 1 && return c[1]
-        c
+        @timeit timer "Agent prepare data" begin
+            actions = actions |> send_to_host
+            # TODO: use alternative to clamp - to run solely on GPU
+            c = clamp.(actions .+ randn(p.rng, p.na) .* repeat([p.act_noise], p.na), -p.act_limit, p.act_limit)
+            p.na == 1 && return c[1]
+            c
+        end
     end
 end
 
 function (agent::Agent)(stage::AbstractStage, env::AbstractEnv, timer::TimerOutput)
-    update!(agent.trajectory, agent.policy, env, stage)
-    update!(agent.policy, agent.trajectory, env, stage)
+    @timeit timer "Agent trajectory" begin
+        update!(agent.trajectory, agent.policy, env, stage)
+    end
+    @timeit timer "Agent policy update" begin
+        if typeof(device(agent)) == Val{:cpu}
+            update!(agent.policy, agent.trajectory, env, stage)
+        else
+            CUDA.@sync begin
+                update!(agent.policy, agent.trajectory, env, stage)
+            end
+        end
+    end
 end
 
 function (agent::Agent)(stage::PreExperimentStage, env::AbstractEnv, timer::TimerOutput)
-    update!(agent.policy, agent.trajectory, env, stage)
+    @timeit timer "Agent policy update" begin
+        if typeof(device(agent)) == Val{:cpu}
+            update!(agent.policy, agent.trajectory, env, stage)
+        else
+            CUDA.@sync begin
+                update!(agent.policy, agent.trajectory, env, stage)
+            end
+        end
+    end
 end
 
 function (agent::Agent)(stage::PreActStage, env::AbstractEnv, action, timer::TimerOutput)
-    update!(agent.trajectory, agent.policy, env, stage, action)
-    update!(agent.policy, agent.trajectory, env, stage)
+    @timeit timer "Agent trajectory" begin
+        update!(agent.trajectory, agent.policy, env, stage, action)
+    end
+    @timeit timer "Agent policy update" begin
+        if typeof(device(agent)) == Val{:cpu}
+            update!(agent.policy, agent.trajectory, env, stage)
+        else
+            CUDA.@sync begin
+                update!(agent.policy, agent.trajectory, env, stage)
+            end
+        end
+    end
 end
 
 function run(policy::AbstractPolicy,
     env::AbstractEnv,
+    timer::TimerOutput,
     stop_condition = StopAfterEpisode(1),
     hook = EmptyHook(),
-    timer::TimerOutput)
+    )
 
     hook(PRE_EXPERIMENT_STAGE, policy, env)
     policy(PRE_EXPERIMENT_STAGE, env, timer)
@@ -58,7 +110,13 @@ function run(policy::AbstractPolicy,
             policy(PRE_ACT_STAGE, env, action, timer)
             hook(PRE_ACT_STAGE, policy, env, action)
 
-            env(action)
+            @timeit timer "Env calculation" begin
+                if env.Ad isa CuArray
+                    env(CuArray(action))
+                else
+                    env(action)
+                end
+            end
 
             policy(POST_ACT_STAGE, env, timer)
             hook(POST_ACT_STAGE, policy, env)
