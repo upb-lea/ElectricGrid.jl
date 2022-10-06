ddbe9d87149a785ed01377333f365218b57d555f
Marvin Meyer <meyer.marvin95@web.de>
2022-10-06 11:12:42 +0200

    #23 Implementation of the PV array which consits of PV modules.

--- src/pv_module.jl
+++ src/pv_module.jl
@@ -9,15 +9,27 @@ Base.@kwdef mutable struct PV_module
     k = 1.3806e-23             # Boltzman constant
     q = 1.6022e-19             # Electron charge
     T_0 = 273                  # Temperature offset
-    N_cell = 1                 # Number of cells
+    N_cell = 36                # Number of cells
     
     G_ref = 1000               # Irradiance at STC
     mu_sc = 1.3e-3             # Coefficient tempreture of short circuit current
     I_ph_ref = 3.11            # Phototcurrent at STC
     
+    
 end;
 
-function get_I(self::PV_module, V, G, T)
+Base.@kwdef mutable struct PV_array
+    pv_module::PV_module           # module parameters
+    N_modules = 20
+    connection = 's'
+    
+end;
+
+
+
+function get_I(pv_arr::PV_array, V, G, T)
+
+    self = pv_arr.pv_module
     function I_photo(self::PV_module, G, T)
         dT = self.T_0 + T
         I_ph = G/self.G_ref*(self.I_ph_ref + self.mu_sc * dT)
@@ -27,23 +39,25 @@ function get_I(self::PV_module, V, G, T)
     function I_diode(self::PV_module, V, G, T)
         dT = self.T_0 + T
         V_T = self.k*dT/self.q
-        I_d = self.I_0*(exp(V/(self.ni*self.N_cell*V_T))-1)
+        I_d = self.I_0*(exp(V/(self.ni*self.N_cell*pv_arr.N_modules*V_T))-1)
         return I_d
     end;
     
-    I = I_photo(self, G, T) - I_diode(self, V, G, T)
+    I = (I_photo(self, G, T) - I_diode(self, V, G, T)) 
     return I
 end
 
-function get_V(self::PV_module, I, G, T)
+function get_V(pv_arr::PV_array, I, G, T)
+
+    self = pv_arr.pv_module
     function I_photo(self::PV_module, G, T)
         dT = self.T_0 + T
         I_ph = G/self.G_ref*(self.I_ph_ref + self.mu_sc * dT)
         return I_ph
     end;
     
-    I = maximum([0,I])
-    res = (I_photo(self, G, T)-I)
+    I = maximum([0.0,I[1]])
+    res = I_photo(self, G, T)-I
     
     dT = self.T_0 + T
     V_T = self.k*dT/self.q
@@ -51,7 +65,7 @@ function get_V(self::PV_module, I, G, T)
     if res <= 0
         V=0
     else
-        V = self.ni*self.N_cell*V_T*(log((res)/self.I_0)+1)
+        V = self.ni*self.N_cell*pv_arr.N_modules*V_T*(log((res)/self.I_0)+1)
     end
     
     return V
