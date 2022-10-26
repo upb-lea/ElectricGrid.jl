86972aced2a1623d1f833be6fb241ad61960f88c
SeptimusBoshoff <septimus@global.co.za>
2022-10-26 16:35:20 +0200

    save

--- examples/Mega_Control_Example.jl
+++ examples/Mega_Control_Example.jl
@@ -1,4 +1,5 @@
 using Dare
+using ReinforcementLearning
 
 function reference(t)
     
@@ -215,7 +216,7 @@ plt_state_ids = []#"source1_v_C_a", "source2_v_C_a", "source1_i_L1_a", "source2_
 plt_action_ids = []#"u_v1_a", "u_v1_b", "u_v1_c"]
 hook = DataHook(collect_state_ids = plt_state_ids, collect_action_ids = plt_action_ids, 
 collect_vrms_idx = [1 2], collect_irms_idx = [1 2], collect_pq_idx = [1 2],
-save_best_NNA = true, collect_reference = false, plot_rewards=false)
+save_best_NNA = false, collect_reference = false, plot_rewards=false)
 
 run(ma, env, StopAfterEpisode(1), hook);
 
