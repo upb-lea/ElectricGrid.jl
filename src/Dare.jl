24a02353f0838e6d57c650a498bcc80e22237008
Jan Stenner <jan.stenner@uni-paderborn.de>
2022-10-26 16:35:18 +0200

    fixes and cleanup

--- src/Dare.jl
+++ src/Dare.jl
@@ -5,25 +5,6 @@ using PlotlyJS
 
 #export create_setup, Classical_Policy, create_agent_ddpg, Source_Initialiser, MultiAgentGridController, DataHook, plot_hook_results, plot_best_results, NodeConstructor, dare_setup, SimEnv
 
-#code to export all, taken from https://discourse.julialang.org/t/exportall/4970/18
-macro make_public(module_name::Symbol)
-
-    eval(Meta.parse("export " * string(module_name)))
-
-    as_module = eval(module_name)
-    @assert as_module isa Module
-
-    for name in names(as_module; all = true)
-        if (string(name)[1] != '#')
-            #println("export " * string(name))
-            as_module.eval(Meta.parse("export " * string(name)))
-        end
-    end
-
-    return nothing
-end
-export make_public
-@make_public Dare
 
 include("./nodeconstructor.jl")
 include("./env.jl")
@@ -34,5 +15,12 @@ include("./MultiAgentGridController.jl")
 include("./plotting.jl")
 include("./data_hook.jl")
 include("./Dare_Wrapper.jl")
+
+#code to export all, taken from https://discourse.julialang.org/t/exportall/4970/18
+for n in names(@__MODULE__; all=true)
+    if Base.isidentifier(n) && n ∉ (Symbol(@__MODULE__), :eval, :include)
+        @eval export $n
+    end
+end
 
 end # module
