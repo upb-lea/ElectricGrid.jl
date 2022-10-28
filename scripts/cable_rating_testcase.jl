f1000ed2c61b5c73fe930fe984a23e7c8f57679e
webbah <danielweber.leo@gmx.de>
2022-10-28 11:39:31 +0200

    Power Flow (Cable Ratings) #24 - finished an double-checked automated Y_bus calulation

--- scripts/cable_rating_testcase.jl
+++ scripts/cable_rating_testcase.jl
@@ -16,27 +16,32 @@ CM = [ 0. 0. 1.
         0. 0. 2
         -1. -2. 0.]
 
+CM = [ 0. 0. 1.
+        0. 0. 2
+        -1. -2. 0.]
+
 parameters = Dict{Any, Any}(
     # generate 2 sources, each 10 kVA with autodesigned filter
     "source" => Any[
                     Dict{Any, Any}("pwr"=>10000.0, "fltr"=>"LC", "source_type" => "ideal"),
+                    Dict{Any, Any}("pwr"=>10000.0, "fltr"=>"L", "source_type" => "ideal"),
                     Dict{Any, Any}("pwr"=>10000.0, "fltr"=>"L", "source_type" => "ideal")
                     ],
     # TODO: what if active load? 
     "load"   => Any[
                     Dict{Any, Any}("R"=>14, "impedance"=>"R")
-                    #Dict{Any, Any}("C"=>0.381, "L"=>0.2, "R"=>14, "impedance"=>"RLC")
+                    Dict{Any, Any}("C"=>0.381, "L"=>0.2, "R"=>14, "impedance"=>"RLC")
                     ],
     # desing the cables underrated
-    "cable"  => Any[
-                    Dict{Any, Any}("C"=>4.0e-7, "L"=>0.000264, "R"=>0.722),
-                    Dict{Any, Any}("C"=>4.0e-7, "L"=>0.000264, "R"=>0.722)
-                    ],
+    #"cable"  => Any[
+    #                Dict{Any, Any}("C"=>4.0e-7, "L"=>0.000264, "R"=>0.722),
+    #                Dict{Any, Any}("C"=>4.0e-7, "L"=>0.000264, "R"=>0.722)
+    #                ],
     "grid"   => Dict{Any, Any}("fs"=>10000.0, "phase"=>1, "v_rms"=>230)
 )
 
 
-env = SimEnv(num_sources = 2, num_loads = 1, CM = CM, parameters = parameters, maxsteps=600)
+env = SimEnv(num_sources = 5, num_loads = 4, CM = nothing, parameters = parameters, maxsteps=600)
 
 #################################
 # 2 Calulate characteristic impedance Z_0 of cables
@@ -69,19 +74,86 @@ P_max = Dict{Any, Any}(
 # per hand - to be automated based on CM
 # which cable number belongs to which source?
 omega = 2*π*50  # TODO: env.nc.parameters["fg"]??
-G_RL = env.nc.parameters["cable"][1]["R"] / (env.nc.parameters["cable"][1]["R"]^2 - omega^2 *env.nc.parameters["cable"][1]["L"]^2)
-B_RL = (omega *env.nc.parameters["cable"][1]["L"])/ (env.nc.parameters["cable"][1]["R"]^2 - omega^2 *env.nc.parameters["cable"][1]["L"]^2)
+#=
+cable_idx = 1
+G_RL_1 = env.nc.parameters["cable"][cable_idx]["R"] / (env.nc.parameters["cable"][cable_idx]["R"]^2 - omega^2 *env.nc.parameters["cable"][1]["L"]^2)
+B_RL_1 = (omega * env.nc.parameters["cable"][cable_idx]["L"])/ (env.nc.parameters["cable"][cable_idx]["R"]^2 - omega^2 *env.nc.parameters["cable"][1]["L"]^2)
 
-Y_11 = env.nc.parameters["cable"][1]["C"] / 2 # C is splitted to 2 capacitors
+B_shunt_1 = omega * env.nc.parameters["cable"][cable_idx]["C"] / 2 
+
+cable_idx = 2
+G_RL_2 = env.nc.parameters["cable"][cable_idx]["R"] / (env.nc.parameters["cable"][cable_idx]["R"]^2 - omega^2 *env.nc.parameters["cable"][1]["L"]^2)
+B_RL_2 = (omega *env.nc.parameters["cable"][cable_idx]["L"])/ (env.nc.parameters["cable"][cable_idx]["R"]^2 - omega^2 *env.nc.parameters["cable"][1]["L"]^2)
+
+B_shunt_2 = omega * env.nc.parameters["cable"][cable_idx]["C"] / 2 
+
+
+# Load is always parrallel connection of devices
+env.nc.parameters["load"]
+
+Y_11 = G_RL_1 + B_RL_1*im  + B_shunt_1*im # C is splitted to 2 capacitors
 Y_12 = 0
-Y_13 = G_RL + B_RL*im
-Y_22 = env.nc.parameters["cable"][1]["C"] / 2 # C is splitted to 2 capacitors
+Y_13 = G_RL_1 + B_RL_1*im 
+Y_22 = G_RL_2 + B_RL_2*im + B_shunt_2*im # C is splitted to 2 capacitors
 Y_21 = 0
-Y_23 = G_RL + B_RL*im
-Y_32 = G_RL + B_RL*im
-Y_31 = G_RL + B_RL*im
-Y_33 = env.nc.parameters["cable"][1]["C"] # 2 times!
+Y_23 = G_RL_2 + B_RL_2*im
+Y_32 = G_RL_2 + B_RL_2*im
+Y_31 = G_RL_1 + B_RL_1*im
+Y_33 = G_RL_1 + B_RL_1*im + G_RL_2 + B_RL_2*im  + B_shunt_1*im + + B_shunt_2*im 
 
-Y_bus = [[Y_11, Y_12, Y_13],
-         [Y_21, Y_22, Y_23],
-         [Y_31, Y_32, Y_33]]
+Y_bus_fixed = [[Y_11, Y_12, Y_13],
+               [Y_21, Y_22, Y_23],
+               [Y_31, Y_32, Y_33]]
+
+G_bus = real(Y_bus)
+B_bus = imag(Y_bus)
+=#
+
+################################# 
+# Automated Y_bus calculation, to be shifted to the NC
+# 2 sources, 1 load - in CM, first all sources, then all loads are listed
+# 1 Bus per component -> 3 here. 
+#       S1  S2 L1
+#  S1  [ 0. 0. 1.
+#  S2    0. 0. 2
+#  L1   -1. -2. 0.]
+
+
+
+num_buses = env.nc.tot_ele  # Y -> num_buses x num_buses
+Y_bus = zeros(Complex{Float64}, num_buses, num_buses)
+
+for col in 1:num_buses
+    for row in 1:num_buses
+
+        if env.nc.CM[row, col] != 0  # we have a cable connected
+            # CM index defines the number of the cable
+            cable_idx = abs(Int(env.nc.CM[row, col]))
+            #println(cable_idx)
+            #println(row)
+            #println(col)
+            G_RL = env.nc.parameters["cable"][cable_idx]["R"] / (env.nc.parameters["cable"][cable_idx]["R"]^2 + omega^2 *env.nc.parameters["cable"][1]["L"]^2)
+            B_RL = (omega *env.nc.parameters["cable"][cable_idx]["L"])/ (env.nc.parameters["cable"][cable_idx]["R"]^2 + omega^2 *env.nc.parameters["cable"][1]["L"]^2)
+            Y_bus[row, col] = -G_RL - im*B_RL
+        
+        elseif row == col  # diagonal elements
+            # Go through all col elements of that row to find the connected cable to that bus (non zero elements in CM[:, row])
+            println(env.nc.CM[row, :])
+            cable_idxs = filter(n -> n !=0, env.nc.CM[row, :])
+            G = 0
+            B = 0
+            for idx in cable_idxs
+                # add all RL 
+                idx = abs(Int(idx))
+                G += env.nc.parameters["cable"][idx]["R"] / (env.nc.parameters["cable"][idx]["R"]^2 + omega^2 *env.nc.parameters["cable"][1]["L"]^2)
+                B += (omega *env.nc.parameters["cable"][idx]["L"])/ (env.nc.parameters["cable"][idx]["R"]^2 + omega^2 *env.nc.parameters["cable"][1]["L"]^2)
+                # and add all shunt C connected to that bus since diagonal element
+                B += omega * env.nc.parameters["cable"][idx]["C"] / 2 
+            end
+            println(G)
+            println(B)
+            println(G + im*B)
+            Y_bus[row, col] = G + im*B
+        end
+    end
+end
