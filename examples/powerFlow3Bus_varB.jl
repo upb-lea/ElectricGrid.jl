e33ec3ea72f14bbf472c3eeb02973f622b423404
webbah <danielweber.leo@gmx.de>
2022-11-21 14:01:25 +0100

    Power Flow (Cable Ratings) #24 b_var2

--- examples/powerFlow3Bus_varB.jl
+++ examples/powerFlow3Bus_varB.jl
@@ -38,12 +38,13 @@ G = [[2*G1, -G1, -G1],
      [-G1, -G1, 2*G1]]
 
 b_variable = 1e9
+b_variable2 = 1e9
 
 #B = [[2*B1, -B1, -B1],
 #B = [[B1+b_variable, b_variable, -B1],
- B = [[B1+b_variable, -b_variable, -B1],
+ B = [[b_variable2+b_variable, -b_variable, -b_variable2],
       [-b_variable,          2*B1, -B1],
-      [-B1,                  -B1, 2*B1]]
+      [-b_variable2,                  -B1, 2*B1]]
 
 model = Model(Ipopt.Optimizer)
 
@@ -55,15 +56,16 @@ model = Model(Ipopt.Optimizer)
 @variable(model, -0.1 <= theta3 <= 0.1)
 @variable(model, 490 <= QG_3 <= 500)
 @variable(model, -2.1 <= b_var <= 100.0)
+@variable(model, -2.1 <= b_var2 <= 100.0)
 
 # non-linear objectives
-@NLobjective(model, Min, abs(PG_1)/200 + abs(QG_1)/500 + abs(v2)/240 + abs(theta2)/0.1 + abs(theta3)/0.1 + abs(QG_3)/700 + abs(b_var)/1.5)
+@NLobjective(model, Min, abs(PG_1)/200 + abs(QG_1)/500 + abs(v2)/240 + abs(theta2)/0.1 + abs(theta3)/0.1 + abs(QG_3)/700 + abs(b_var)/1.5 + abs(b_var2)/1.5)
 
 
 @NLconstraint(model, P_Bus1,
-    v1 * v1 * (G[1][1] * cos(theta1 - theta1) + (B[1][1] + b_var) * sin(theta1 - theta1)) + 
+    v1 * v1 * (G[1][1] * cos(theta1 - theta1) + (b_var2 + b_var) * sin(theta1 - theta1)) + 
     v1 * v2 * (G[1][2] * cos(theta1 - theta2) - b_var * sin(theta1 - theta2)) +
-    v1 * v3 * (G[1][3] * cos(theta1 - theta3) + B[1][3] * sin(theta1 - theta3)) 
+    v1 * v3 * (G[1][3] * cos(theta1 - theta3) + b_var2 * sin(theta1 - theta3)) 
     == PG_1 - PL_1)
 
 @NLconstraint(model, P_Bus2,
@@ -73,13 +75,13 @@ model = Model(Ipopt.Optimizer)
     == PG_2 - PL_2)
 
 @NLconstraint(model, P_Bus3,
-    v3 * v1 * (G[3][1] * cos(theta3 - theta1) + B[3][1] * sin(theta3 - theta1)) + 
+    v3 * v1 * (G[3][1] * cos(theta3 - theta1) - b_var2 * sin(theta3 - theta1)) + 
     v3 * v2 * (G[3][2] * cos(theta3 - theta2) + B[3][2] * sin(theta3 - theta2)) +
     v3 * v3 * (G[3][3] * cos(theta3 - theta3) + B[3][3] * sin(theta3 - theta3)) 
     == PG_3 - PL_3)
 
 @NLconstraint(model, Q_Bus1,
-    v1 * v1 * (G[1][1] * sin(theta1 - theta1) - (B[1][1] + b_var) * cos(theta1 - theta1)) + 
+    v1 * v1 * (G[1][1] * sin(theta1 - theta1) - (b_var2 + b_var) * cos(theta1 - theta1)) + 
     v1 * v2 * (G[1][2] * sin(theta1 - theta2) + b_var * cos(theta1 - theta2)) +
     v1 * v3 * (G[1][3] * sin(theta1 - theta3) - B[1][3] * cos(theta1 - theta3))
     == QG_1 - QL_1)
@@ -91,7 +93,7 @@ model = Model(Ipopt.Optimizer)
     == QG_2 - QL_2)
 
 @NLconstraint(model, Q_Bus3,
-    v3 * v1 * (G[3][1] * sin(theta3 - theta1) - B[3][1] * cos(theta3 - theta1)) + 
+    v3 * v1 * (G[3][1] * sin(theta3 - theta1) + b_var2 * cos(theta3 - theta1)) + 
     v3 * v2 * (G[3][2] * sin(theta3 - theta2) - B[3][2] * cos(theta3 - theta2)) + 
     v3 * v3 * (G[3][3] * sin(theta3 - theta3) - B[3][3] * cos(theta3 - theta3))
     == QG_3 - QL_3)
@@ -109,6 +111,6 @@ termination_status = $(termination_status(model))
 primal_status      = $(primal_status(model))
 objective_value    = $(objective_value(model))
 """)
-println("PG1, QG1, v2, theta2, theta3, QG_3, b_var")
-println("$(value(PG_1)), $(value(QG_1)), $(value(v2)), $(value(theta2)), $(value(theta3)), $(value(QG_3)), $(value(b_var))")
+println("PG1, QG1, v2, theta2, theta3, QG_3, b_var, b_var2")
+println("$(value(PG_1)), $(value(QG_1)), $(value(v2)), $(value(theta2)), $(value(theta3)), $(value(QG_3)), $(value(b_var)), $(value(b_var2))")
 
