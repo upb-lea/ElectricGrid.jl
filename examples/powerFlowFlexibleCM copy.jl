6f584dc04e5327d8b0a65789a976a5c37c8a9fcb
cvikas <cvikas@mail.uni-paderborn.de>
2022-12-01 13:17:39 +0100

    #24 implement R, L, C as function(radius)

--- examples/powerFlowFlexibleCM copy.jl
+++ examples/powerFlowFlexibleCM copy.jl
@@ -99,25 +99,79 @@ P_node = Array{NonlinearConstraintRef, 1}(undef, num_nodes)
 Q_node = Array{NonlinearConstraintRef, 1}(undef, num_nodes)
 
 # As radius goes down resistance goes up, inductance goes up, capacitance goes down. Put in formulas for this.
-@variable(model, cables[1 : num_cables, ["L", "X_R", "C_L"]]) # this is wrong - #TODO ["radius"] for futute : ["radius", "conductivity"] 
+#TODO ["radius"] for futute : ["radius", "conductivity"] 
+@variable(model, cables[1 : num_cables, ["radius"]]) # this is wrong - 
+
+# defining indivdual expressions for R, L, C as a fucntion of radius with length fixed
+#=
+    
+    # L = 0.00025
+    # assumption for single phase two wire system
+    # space b/w conductors
+    
+    # r' = 1e-(1/4)r
+
+    L =  l * 4e-7 * log(D/0.7788 * r)  # m* H/m
+
+    # resistivity remains constant ρ_(T=50) = 1.973e-8 
+    R = l * 1.973e-8 / (π * r^2) # m * Ω/m
+
+    # X_R = 0.38 # ratio of omega*L/R
+    # X_R = omega * L(r)/R(r)
+
+    # line to neutral
+    C = l * 2π * 8.854e-12 / (log(D/r)) #l * F/m 
+=#
+
+# fixed length
+l = 1000 #m
+# fixed distance b/w cables
+D = 0.5 #m
+
+
+L_cable = Array{NonlinearExpression, 1}(undef, num_cables)
+R_cable = Array{NonlinearExpression, 1}(undef, num_cables)
+C_cable = Array{NonlinearExpression, 1}(undef, num_cables)
 cable_conductance = Array{NonlinearExpression, 1}(undef, num_cables)
 cable_susceptance_0 = Array{NonlinearExpression, 1}(undef, num_cables) # diagonals - where we add capacitances
 cable_susceptance_1 = Array{NonlinearExpression, 1}(undef, num_cables) # off diagonals
 
+
 for i=1:num_cables
 
-    set_bounds(cables[i, "L"], 0.00025, 0.00023, 0.00026)
-    set_bounds(cables[i, "X_R"], 0.38, 0.37, 0.4)
-    set_bounds(cables[i, "C_L"], 0.0016, 0.0015, 0.0017)
+    # set_bounds(cables[i, "L"], 0.00025, 0.00023, 0.00026)
+    # set_bounds(cables[i, "X_R"], 0.38, 0.37, 0.4)
+    # set_bounds(cables[i, "C_L"], 0.0016, 0.0015, 0.0017)
 
-    #set_bounds(cables[i, "radius"], 0.0016, 0.1, 0.25(?)) mm #TODO
+    #TODO: AWG 6 - AWG 12 as discussed
+    set_bounds(cables[i, "radius"], (2.05232e-3)/2, (2.05232e-3)/2, (4.1148e-3)/2) #m 
 
-    #R = (omega*L)/X_R
-    #C = C_L*L
-    cable_conductance[i] = @NLexpression(model, ((omega * cables[i, "L"]) / cables[i, "X_R"]) / (((omega*cables[i, "L"]) / cables[i, "X_R"])^2 + omega^2 * cables[i, "L"]^2))
-    cable_susceptance_1[i] = @NLexpression(model, (-omega * cables[i, "L"] / (((omega*cables[i, "L"])/cables[i, "X_R"])^2 + omega^2 * cables[i, "L"]^2)))
-    cable_susceptance_0[i] = @NLexpression(model, (-omega * cables[i, "L"] / (((omega*cables[i, "L"])/cables[i, "X_R"])^2 + omega^2 * cables[i, "L"]^2)) + omega*cables[i, "C_L"]*cables[i, "L"]/2)
+    L_cable[i] = @NLexpression(model, l * 4e-7 * log(D/(0.7788 * cables[i, "radius"])))  # m* H/m
+
+    # resistivity remains constant ρ_(T=50) = 1.973e-8 
+    R_cable[i] = @NLexpression(model, l * 1.973e-8 / (π * cables[i, "radius"]^2)) # m * Ω/m
+
+    # X_R = 0.38 # ratio of omega*L/R
+    # X_R = omega * L(r)/R(r)
+
+    # line to neutral
+    C_cable[i] = @NLexpression(model, l * 2π * 8.854e-12 / (log(D/cables[i, "radius"]))) #m * F/m
+
+    # B1 = -omega * L /(((omega*L)/X_R)^2 + omega^2 * L^2)
+    # B1 = -omega * L(r) / (R(r)^2 + (omega * L(r))^2)
+    # # G1 = ((omega*L)/X_R) /(((omega*L)/X_R)^2 + omega^2 * L^2)
+    # G1 = R(r) / (R(r)^2 + (omega*L(r))^2)
+    # B0 = (-omega * L /(((omega*L)/X_R)^2 + omega^2 * L^2)) + omega*C_L*L
+    # B0 = (-omega*L(r) / (R(r)^2 + (omega*L(r))^2 )) + omega*C(r)  
+
+    # cable_conductance[i] = @NLexpression(model, ((omega * cables[i, "L"]) / cables[i, "X_R"]) / (((omega*cables[i, "L"]) / cables[i, "X_R"])^2 + omega^2 * cables[i, "L"]^2))
+    cable_conductance[i] = @NLexpression(model, (R_cable[i] / ((R_cable[i])^2 + (omega * L_cable[i])^2)))
+    # cable_susceptance_1[i] = @NLexpression(model, (-omega * cables[i, "L"] / (((omega*cables[i, "L"])/cables[i, "X_R"])^2 + omega^2 * cables[i, "L"]^2)))
+    cable_susceptance_1[i] = @NLexpression(model, (-omega * L_cable[i] / (((R_cable[i])^2 + (omega * L_cable[i])^2))))
+    # cable_susceptance_0[i] = @NLexpression(model, (-omega * cables[i, "L"] / (((omega*cables[i, "L"])/cables[i, "X_R"])^2 + omega^2 * cables[i, "L"]^2)) + omega*cables[i, "C_L"]*cables[i, "L"]/2)
+    cable_susceptance_0[i] = @NLexpression(model, (-omega * L_cable[i] / (((R_cable[i])^2 + (omega * L_cable[i])^2)) + C_cable[i]/2))
 end
+
 
 for i in 1:num_nodes
 
@@ -166,16 +220,17 @@ end
 
 cable_constraints = Array{NonlinearConstraintRef, 1}(undef, num_cables)
 # maybe remove this? but add as check after optimisation has been completed.
-for i in 1:num_cables
+# for i in 1:num_cables
 
-    j, k = Tuple(findfirst(x -> x == i, CM))
+#     j, k = Tuple(findfirst(x -> x == i, CM))
 
-    cable_constraints[i] = @NLconstraint(model,
-        abs( nodes[j, "v"] * nodes[k, "v"] * (sin(nodes[j, "theta"] - nodes[k, "theta"]))/(omega*cables[i, "L"])) # this formula is not quite correct - missing resistances and capacitances
-        <= 0.93 * nodes[j, "v"] * nodes[k, "v"] * sqrt(cables[i, "C_L"]) # check if there should be a 2 in the equation
-    )
+#     cable_constraints[i] = @NLconstraint(model,
+#     # fins suitable expression
+#         abs( nodes[j, "v"] * nodes[k, "v"] * (sin(nodes[j, "theta"] - nodes[k, "theta"]))/(omega*L_cable[i])) # this formula is not quite correct - missing resistances and capacitances
+#         <= 0.93 * nodes[j, "v"] * nodes[k, "v"] * sqrt(cables[i, "C_L"]) # check if there should be a 2 in the equation
+#     )
 
-end
+# end
 #0.93 * value(nodes[j, "v"] * nodes[k, "v"]) * sqrt(value(cables[i, "C_L"]))
 # non-linear objectives
 @NLexpression(model, P_source_mean, sum(nodes[j,"P"] for j in 1:num_source) / num_source)
@@ -183,20 +238,24 @@ end
 
 # TODO: normalisation, i.e. weighting between minimising P and Q, and minimising cable radius? Maybe use per unit system
 # normalisation : 1. max value
-#                 2. p.u. 0
+#                 2. p.u. 
+
 # Sbase_1_phase = sum(loads)
 # Vbase_rms = 230
 # Ibase_rms = f(Sbase_1_phase, Vbase_rms)
 # Zbase = f(Vbase_rms, Ibase_rms)
-@NLobjective(model, Min, abs(sum(nodes[i,"P"] for i in 1:num_source))/1000 # replace sum by mean or divide by 1000*num_source
-                        + abs(sum(nodes[i,"Q"] for i in 1:num_source))/1000
-                        + sum(nodes[i,"v"] for i in num_source+1:num_nodes)/230 # TODO: maybe wrong- minimise the deviation of voltage, excluding reference node (which does not neeed to be node 1)
-                        + abs(sum(nodes[i,"theta"] for i in 2:num_nodes))/π
-                        + sum(cables[i, "X_R"] for i in 1:num_cables) # replaced with radius, how do we normalise radius??? idea: upper bound of radius times by number of cables
-                        + sum(1/cables[i, "L"] for i in 1:num_cables)
-                        + sum(cables[i, "C_L"] for i in 1:num_cables)
-                        + sum( (nodes[i,"P"] - P_source_mean)^2 for i in 1:num_source)/num_source 
-                        + sum( (nodes[i,"Q"] - Q_source_mean)^2 for i in 1:num_source)/num_source ) # the variance - not exactly right (but good enough)
+
+@NLobjective(model, Min, abs(sum(nodes[i,"P"]/1000 for i in 1:num_source)) # replace sum by mean or divide by 1000*num_source
+                        + abs(sum(nodes[i,"Q"]/1000 for i in 1:num_source)) # take apparent power -- λ_1  
+                        #  + sum(nodes[i,"v"]/230 for i in num_source+1:num_nodes) # TODO: maybe wrong- minimise the deviation of voltage, excluding reference node (which does not neeed to be node 1)
+                        # + abs(sum(nodes[i,"theta"]/pi for i in 2:num_nodes)) # variances (?)
+                        # + sum(cables[i, "X_R"] for i in 1:num_cables) # replaced with radius, how do we normalise radius??? idea: upper bound of radius times by number of cables
+                        # + sum(1/cables[i, "L"] for i in 1:num_cables)  # -- λ_2 
+                        # + sum(cables[i, "C_L"] for i in 1:num_cables) 
+                        + sum( ((nodes[i,"P"] - P_source_mean)^2)/num_source for i in 1:num_source) 
+                        + sum( ((nodes[i,"Q"] - Q_source_mean)^2)/num_source for i in 1:num_source) 
+                        # + sum( ((cables[i, "radius"] / upper_bound(cables[i, "radius"])) for i in 1:num_cables))
+                        ) # the variance - not exactly right (but good enough)
 
 optimize!(model)
 println("""
@@ -204,7 +263,6 @@ termination_status = $(termination_status(model))
 primal_status      = $(primal_status(model))
 objective_value    = $(objective_value(model))
 """)
-
 
 println()
 println()
