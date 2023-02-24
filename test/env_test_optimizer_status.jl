4065fe84cd7c439fa4441587da5ce5bd755772bb
cvikas <vikasc.nitk@gmail.com>
2023-02-24 12:42:44 +0100

    #28 add infeasible test for optim status

--- test/env_test_optimizer_status.jl
+++ test/env_test_optimizer_status.jl
@@ -5,26 +5,17 @@ using JuMP
 include("utils.jl")
 
 # Simple one source - one load model that optimiser can solve feasibly
-CM = [  
-    0. 1.
-    -1. 0.
-]
-
-
-parameters = Dict{Any, Any}(
-    "source" => Any[
-                    Dict{Any, Any}("pwr" => 200e3, "mode" => 1),
-                    # Dict{Any, Any}("pwr" => 100e3, "mode" => 4),
-                    ],
-    "grid" => Dict{Any, Any}("ramp_end" => 0.04)
-)
-
-
-populate_params(parameters, 100e3, 0.95, 230)
+CM = [
+        0 1
+       -1 0
+    ]
 
 
 @testset "Optimizer status: FEASIBLE" begin
-    env = SimEnv(ts = 0.0001, CM = CM, parameters = parameters, t_end = 0.1, verbosity = 2, action_delay = 1)
+
+    parameters = populate_params(100e3, 0.95)
+
+    env = SimEnv(ts=0.0001, CM=CM, parameters=parameters, t_end=0.1, verbosity=2, action_delay=1)
     # Dare.optimizer_status
 
     @test Dare.optimizer_status["termination_status"] == LOCALLY_SOLVED  #add other feasible enums from https://jump.dev/JuMP.jl/stable/moi/reference/models/#MathOptInterface.TerminationStatusCode
@@ -32,13 +23,15 @@ populate_params(parameters, 100e3, 0.95, 230)
 
 end
 
-@testset "Optimizer status: FEASIBLE" begin
-    populate_params(parameters, 230e3, 0.95, 230)
-    env = SimEnv(ts = 0.0001, CM = CM, parameters = parameters, t_end = 0.1, verbosity = 2, action_delay = 1)
+@testset "Optimizer status: INFEASIBLE" begin
+
+    parameters = populate_params(230e3, 0.95)
+    
+    env = SimEnv(ts=0.0001, CM=CM, parameters=parameters, t_end=0.1, verbosity=2, action_delay=1)
     # Dare.optimizer_status
 
-    @test Dare.optimizer_status["termination_status"] == LOCALLY_SOLVED  #add other feasible enums from https://jump.dev/JuMP.jl/stable/moi/reference/models/#MathOptInterface.TerminationStatusCode
-    @test Dare.optimizer_status["primal_status"] == FEASIBLE_POINT  # add other feasible status from above
+    @test Dare.optimizer_status["termination_status"] == LOCALLY_INFEASIBLE  #add other feasible enums from https://jump.dev/JuMP.jl/stable/moi/reference/models/#MathOptInterface.TerminationStatusCode
+    @test Dare.optimizer_status["primal_status"] == INFEASIBLE_POINT  # add other feasible status from above
 
 end
    
