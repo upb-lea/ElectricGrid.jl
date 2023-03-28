f50ff91a1b247358827c5d310e9e7e095be4f9f5
webbah <danielweber.leo@gmx.de>
2023-03-28 15:01:47 +0200

    simple PFE example

--- examples/scripts/env_example.jl
+++ examples/scripts/env_example.jl
@@ -3,9 +3,9 @@ using JEG
 CM = [0. 1.
     -1. 0.]
 
-S_source = 2e6
+S_source = 1e4
 
-S_load = 3e6
+S_load = 1e2
 pf_load = 1
 v_rms = 230
 R_load, L_load, X, Z = ParallelLoadImpedance(S_load, pf_load, v_rms)
@@ -24,62 +24,5 @@ parameters = Dict{Any, Any}(
     )
 
 
-#env = ElectricGridEnv(CM = CM, parameters = parameters, verbosity = 2)
-env = ElectricGridEnv(num_sources = 2, num_loads = 1)
-
-#env.nc.parameters["cable"][1]["i_limit"] = 10e3
-
-
-
-Multi_Agent = SetupAgents(env)
-Source = Multi_Agent.agents["classic"]["policy"].policy.Source
-
-#_______________________________________________________________________________
-# running the time simulation
-
-#hook = Simulate(Multi_Agent, env)
-
-#hook = DataHook(collect_state_ids = env.state_ids,
-#                collect_action_ids = env.action_ids
-#                #collect_sources  = [1]  # alternative
-#                );
-
-states_to_plot = ["source1_v_C_filt_a", "source1_v_C_filt_b", "source1_v_C_filt_c"]
-
-hook = DataHook(collect_state_ids = states_to_plot)
-
-Simulate(Multi_Agent, env, hook=hook)
-
-#_______________________________________________________________________________
-# Plotting
-
-RenderHookResults(hook = hook,
-                    states_to_plot  = states_to_plot,
-                    actions_to_plot = [])
-#env.state_ids
-
-#env = ElectricGridEnv(num_sources = 1, num_loads = 1)
-
-#reset!(env)
-#env([1])
-#for _ in 1:2
-#    env([1,1,1])
-#end
-
-#println(env.done)
-#=
-hook = DataHook(collect_state_ids = env.state_ids,
-                collect_action_ids = env.action_ids
-                #collect_sources  = [1]  # alternative
-                );
-                =#
-
-#println(env.nc.parameters["cable"][1])
-#=
-Power_System_Dynamics(env, hook, num_episodes = 1)
-
-RenderHookResults(hook = hook,
-                    episode = 1,
-                    states_to_plot  = env.state_ids,
-                    actions_to_plot = env.action_ids)
-=#
+env = ElectricGridEnv(CM = CM, parameters = parameters, verbosity = 2)
+#env = ElectricGridEnv(num_sources = 2, num_loads = 1)
