1538809d13d073be80957966f38113a0b040bbdc
webbah <danielweber.leo@gmx.de>
2023-03-30 21:06:14 +0200

    Documentation #31 deleted env and Rl in the guide section

--- docs/make.jl
+++ docs/make.jl
@@ -8,14 +8,9 @@ makedocs(
         "Welcome" => "index.md",
         "Guide" => [
             "Quick Start" => "quickstart.md",
-            "Configuring the Environment" => "Env_Create.md",
-            "Interaction with the Environment" => "Env_Interaction.md",
             "The Nodeconstructor - Theory" => "NodeConstructor_Theory.md",
             "The Nodeconstructor - Application" => "NodeConstructor_Application.md",
             "Set up Classical Controllers" => "classical.md",
-            "Train an RL againt in the JEG framework - 1" => "RL_Single_Agent.md",
-            "Train an RL againt interacting with a stable grid" => "RL_Classical_Controllers_Merge.md",
-            "Train an RL againt in the JEG framework - 2" => "RL_Complex.md",
             "Contributing" => "dev.md"
         ],
         "Environment" => [
