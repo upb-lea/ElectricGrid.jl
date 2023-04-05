a52c93c298b4353dc52828289320c5ad3a7e5eab
Jan Stenner <jan.stenner@uni-paderborn.de>
2023-04-05 15:01:19 +0200

    make jl update

--- docs/convert_mds.jl
+++ docs/convert_mds.jl
@@ -1,17 +1,19 @@
 using Glob
 
 function process_file(filepath::AbstractString)
-    edit_block = false
+    edit_latex_block = false
+    original_lines = []
     output_lines = []
     prev_line = ""
 
     open(filepath) do file
         for line in eachline(file)
+            push!(original_lines, line)
 
             #check for beginning of unencapsulated Latex blocks
-            if occursin(r"\\begin", line)
+            if occursin(r"\\begin{alig", line) || occursin(r"\\begin{equa", line)
                 if !occursin("```math", prev_line)
-                    edit_block = true
+                    edit_latex_block = true
                     push!(output_lines, "```math")
                 end
             end
@@ -21,13 +23,16 @@ function process_file(filepath::AbstractString)
             regex_pattern = r"[^]*?m"
             line = replace(line, regex_pattern => "")
 
+            #change figures/ to assets/
+            line = replace(line, "figures/" => "assets/")
+
             push!(output_lines, line)
 
             #check for end of unencapsulated Latex blocks
-            if occursin(r"\\end", line)
-                if edit_block
+            if occursin(r"\\end{alig", line) || occursin(r"\\end{equa", line)
+                if edit_latex_block
                     push!(output_lines, "```")
-                    edit_block = false
+                    edit_latex_block = false
                 end
             end
 
@@ -35,9 +40,11 @@ function process_file(filepath::AbstractString)
         end
     end
 
-    open(filepath, "w") do file
-        for line in output_lines
-            println(file, line)
+    if output_lines != original_lines
+        open(filepath, "w") do file
+            for line in output_lines
+                println(file, line)
+            end
         end
     end
 end
