7fb9fc65564828d9add88d296fed0d6a034c44dc
Jan Stenner <jan.stenner@uni-paderborn.de>
2023-04-27 15:07:04 +0200

    views test

---
+++ performance_test/views_test.jl
@@ -0,0 +1,41 @@
+using TimerOutputs
+
+timer = TimerOutput()
+
+mutable struct Abc
+    matrix
+end
+
+abc = Abc(rand(2,3,4))
+
+function roll1(abc)
+    for i = 1:10_000
+        abc.matrix[2, :, 1:end-1] = abc.matrix[2, :, 2:end]
+    end
+end
+
+function roll2(matrix)
+    for i = 1:10_000
+        @views matrix[2, :, 1:end-1] = matrix[2, :, 2:end]
+    end
+end
+
+roll1(abc)
+roll2(abc.matrix)
+
+abc = Abc(rand(2,3,4))
+@timeit timer "Run1" begin
+    roll1(abc)
+end
+
+abc = Abc(rand(2,3,4))
+@timeit timer "Run2" begin
+    roll2(abc.matrix)
+end
+
+show(timer)
+
+
+function roll(matrix, ns)
+    @views matrix[ns, :, 1:end-1] = matrix[ns, :, 2:end]
+end
