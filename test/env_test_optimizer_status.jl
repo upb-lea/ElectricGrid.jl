27b2d9cb1cceacd4b4ea119cf54b3090d4c1cf8d
cvikas <vikasc.nitk@gmail.com>
2023-09-22 09:17:41 +0200

    #45 extended tests

--- test/env_test_optimizer_status.jl
+++ test/env_test_optimizer_status.jl
@@ -33,7 +33,12 @@ end
 
     parameters = PopulateParams(230e3, 0.95)
 
-    env = ElectricGridEnv(ts=0.0001, CM=CM, parameters=parameters, t_end=0.1, verbosity=2, action_delay=1)
+    env = ElectricGridEnv(ts=0.0001, 
+                            CM=CM, 
+                            parameters=parameters, 
+                            t_end=0.1, 
+                            verbosity=2, 
+                            action_delay=1)
 
     @test ElectricGrid.optimizer_status["termination_status"] == LOCALLY_INFEASIBLE  #add other feasible enums from https://jump.dev/JuMP.jl/stable/moi/reference/models/#MathOptInterface.TerminationStatusCode
     @test ElectricGrid.optimizer_status["primal_status"] == INFEASIBLE_POINT  # add other feasible status from above
@@ -53,7 +58,7 @@ end
                             action_delay=1)
 
     # @test 
-    ElectricGrid.optimizer_status["termination_status"] == LOCALLY_SOLVED  #add other feasible enums from https://jump.dev/JuMP.jl/stable/moi/reference/models/#MathOptInterface.TerminationStatusCode
+    @test ElectricGrid.optimizer_status["termination_status"] == LOCALLY_SOLVED  #add other feasible enums from https://jump.dev/JuMP.jl/stable/moi/reference/models/#MathOptInterface.TerminationStatusCode
     @test ElectricGrid.optimizer_status["primal_status"] == FEASIBLE_POINT  # add other feasible status from above
 
 end
@@ -71,7 +76,7 @@ end
                             action_delay=1)
 
     # @test 
-    ElectricGrid.optimizer_status["termination_status"] == LOCALLY_INFEASIBLE  #add other feasible enums from https://jump.dev/JuMP.jl/stable/moi/reference/models/#MathOptInterface.TerminationStatusCode
+    @test ElectricGrid.optimizer_status["termination_status"] == LOCALLY_INFEASIBLE  #add other feasible enums from https://jump.dev/JuMP.jl/stable/moi/reference/models/#MathOptInterface.TerminationStatusCode
     @test ElectricGrid.optimizer_status["primal_status"] == INFEASIBLE_POINT  # add other feasible status from above
 
 end
