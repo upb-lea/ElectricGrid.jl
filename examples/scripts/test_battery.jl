250be193f42be572f0d07bbf9d72ef6c0228e490
MarvinMeyer <25954606+MarvinMeyer@users.noreply.github.com>
2023-10-04 13:23:49 +0200

    Hotfix example scripts (#133)
    * Update test_battery.
    
    * Added comments and formatting to scripts.
    
    * Deleted DEMO from nb names
    
    * Changed the plotting function

--- examples/scripts/test_battery.jl
+++ examples/scripts/test_battery.jl
@@ -2,13 +2,19 @@ using ElectricGrid
 using LinearAlgebra
 using DataFrames
 using PlotlyJS
+"""
+This scipt contains parts of the DC_linkModels.ipynb notebook.
+For comments and more documentation see refering notebookin examples/notebooks.
+"""
 
+# constructing a simple grid
 CM = [ 0. 1.
       -1. 0.]
 
+# create suitable load
 R_load, L_load, X, Z = ParallelLoadImpedance(5e3, .95, 230)
 
-
+# configer the other parameters
 parameters = Dict{Any,Any}(
     "source" => Any[
         Dict{Any,Any}("source_type" => "battery", "pwr" => 15000.0, "fltr" => "L",
@@ -22,25 +28,23 @@ parameters = Dict{Any,Any}(
     "grid" => Dict{Any,Any}("fs" => 1e4, "phase" => 3, "v_rms" => 230, "f_grid" => 50, "ramp_end" => 0.9)
 )
 
-
-states = []
-states_all = []
-Charging = []
-Discharging = []
-V_dc_char = []
-V_dc_dis = []
+# create env
 env = ElectricGridEnv(num_sources=1, num_loads=1, CM=CM, parameters=parameters, t_end=2, verbosity=2)
 
+#create agent
 Multi_Agent = SetupAgents(env)
 
+# define information we like to collect
 hook = DataHook(collect_state_ids = [],
                 collect_action_ids = [],
                 collect_vdc_ids = [1],
                 collect_soc_ids = [1],
                 collect_idc_ids = [1],)
 
+# run experiment
 hook = Simulate(Multi_Agent, env, hook=hook)
 
+# render results
 p = RenderHookResults(hook = hook,
                     states_to_plot  = [],
                     vdc_to_plot     = [1],
